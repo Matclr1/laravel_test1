14d13d32c476c187f700beace5ade32092d0d544
Matclr1 <goossematteo@gmail.com>
2026-06-07 20:58:13 +0200

    completer controller avec destroy et chnage candidatures en candidature pour laravel

--- app/Http/Controllers/CandidatureController.php
+++ app/Http/Controllers/CandidatureController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Candidatures;
+use App\Models\Candidature;
 use Illuminate\Http\Request;
 
 class CandidatureController extends Controller{
 
         public function index(){
-            $cand=Candidatures::all();
-            $entretiens=Candidatures::where('status', 'Entretien')->count();
-            return view('candidatures.index', ['candidatures' => $cand, 'entretiens' => $entretiens]);
+            $candidature=Candidature::all();
+            $entretiens=Candidature::where('status', 'Entretien')->count();
+            return view('candidatures.index', ['candidatures' => $candidature, 'entretiens' => $entretiens]);
           
         }
 
@@ -18,34 +18,37 @@ class CandidatureController extends Controller{
             return view('candidatures.create');
         }
         
-        public function show(Candidatures $cand){
-            return view('candidatures.show', ['candidature' => $cand]);
+        public function show(Candidature $candidature){
+            return view('candidatures.show', ['candidature' => $candidature]);
         }
 
         public function store(Request $request){
-            $cand= new Candidatures();
-            $cand->company = $request->input('company');
-            $cand->position = $request->input('position');
-            $cand->status = $request->input('status');
-            $cand->applied_at = $request->input('applied_at');
-            $cand->save();
+            $candidature= new Candidature();
+            $candidature->company = $request->input('company');
+            $candidature->position = $request->input('position');
+            $candidature->status = $request->input('status');
+            $candidature->applied_at = $request->input('applied_at');
+            $candidature->save();
 
             return redirect()->route('candidatures.index');
         }
 
-        public function edit(Candidatures $cand){
-            return view('candidatures.edit', ['candidature' => $cand]);
+        public function edit(Candidature $candidature){
+            return view('candidatures.edit', ['candidature' => $candidature]);
         }
 
-        public function update(Request $request, Candidatures $cand){
-            $cand->company= $request->input('company');
-            $cand->position = $request->input('position');
-            $cand->status = $request->input('status');
-            $cand->applied_at = $request->input('applied_at');
-            $cand->save();
+        public function update(Request $request, Candidature $candidature){
+            $candidature->company= $request->input('company');
+            $candidature->position = $request->input('position');
+            $candidature->status = $request->input('status');
+            $candidature->applied_at = $request->input('applied_at');
+            $candidature->save();
 
-            return redirect()->route('candidatures.show', ['candidature'=> $cand->id]);
+            return redirect()->route('candidatures.show', ['candidature'=> $candidature->id]);
 
         }
-
+            public function destroy(Candidature $candidature){
+                $candidature->delete();
+                return redirect()->route('candidatures.index');
+            }
 };
